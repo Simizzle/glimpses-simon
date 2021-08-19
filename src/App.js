1607b6098c4dff6f830e4ffba1094e54c4e4fe02
Simon Morgan <simonmakessound@gmail.com>
2021-08-19 15:29:19 +0100

    even more updates

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () =>  {
       setBool(true);
       fetchPosts();
       authUser(setUser)
-    }, [bool]);
+    }, [bool, fetchPosts]);
       return (
         <AppContainer>
           {user ? <Redirect to= '/profile'/> : <Redirect to = '/'/>}
